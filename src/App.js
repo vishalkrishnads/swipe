41ab45bba29fde13b8b7118661cd744832552625
vishalkrishnads <321vishalds@gmail.com>
2023-10-28 15:01:27 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import InvoiceForm from './components/InvoiceForm';
+import Home from './components/Home';
+import { Routes, Route } from 'react-router-dom';
+import Editor from './components/InvoiceForm';
 
-class App extends Component {
-  render() {
-  return (
+const App = () => {
+  return (    
     <div className="App d-flex flex-column align-items-center justify-content-center w-100">
       <Container>
-        <InvoiceForm/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor" element={<Editor />} />
+        </Routes>
       </Container>
     </div>
   );
-}}
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import store from './redux/store'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store} >
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
